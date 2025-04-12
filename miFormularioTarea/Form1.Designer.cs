003dabcf28b93d644893fb8f5a18ec2e6b0c3aea
Jhony-De-Leon <jdeleonp29@miumg.edu.gt>
2025-04-11 20:33:36 -0600

    Tarea sobre Crud y formulario

---
+++ miFormularioTarea/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace miFormularioTarea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxCarnet = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxEstudiante = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxSeccion = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxEmail = new System.Windows.Forms.TextBox();
+            this.buttonBuscar = new System.Windows.Forms.Button();
+            this.buttonCrear = new System.Windows.Forms.Button();
+            this.buttonSaludar = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxNotas = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(52, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Carnet";
+            // 
+            // textBoxCarnet
+            // 
+            this.textBoxCarnet.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxCarnet.Location = new System.Drawing.Point(134, 34);
+            this.textBoxCarnet.Name = "textBoxCarnet";
+            this.textBoxCarnet.Size = new System.Drawing.Size(207, 27);
+            this.textBoxCarnet.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(52, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 22);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombre";
+            // 
+            // textBoxEstudiante
+            // 
+            this.textBoxEstudiante.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEstudiante.Location = new System.Drawing.Point(134, 90);
+            this.textBoxEstudiante.Name = "textBoxEstudiante";
+            this.textBoxEstudiante.Size = new System.Drawing.Size(525, 27);
+            this.textBoxEstudiante.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(53, 135);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Seccion";
+            // 
+            // textBoxSeccion
+            // 
+            this.textBoxSeccion.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSeccion.Location = new System.Drawing.Point(136, 135);
+            this.textBoxSeccion.Name = "textBoxSeccion";
+            this.textBoxSeccion.Size = new System.Drawing.Size(100, 27);
+            this.textBoxSeccion.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(53, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(66, 22);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Correo";
+            // 
+            // textBoxEmail
+            // 
+            this.textBoxEmail.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxEmail.Location = new System.Drawing.Point(134, 181);
+            this.textBoxEmail.Name = "textBoxEmail";
+            this.textBoxEmail.Size = new System.Drawing.Size(220, 27);
+            this.textBoxEmail.TabIndex = 7;
+            // 
+            // buttonBuscar
+            // 
+            this.buttonBuscar.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonBuscar.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBuscar.Location = new System.Drawing.Point(366, 30);
+            this.buttonBuscar.Name = "buttonBuscar";
+            this.buttonBuscar.Size = new System.Drawing.Size(80, 37);
+            this.buttonBuscar.TabIndex = 8;
+            this.buttonBuscar.Text = "Buscar";
+            this.buttonBuscar.UseVisualStyleBackColor = false;
+            this.buttonBuscar.Click += new System.EventHandler(this.buttonBuscar_Click_1);
+            // 
+            // buttonCrear
+            // 
+            this.buttonCrear.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonCrear.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCrear.Location = new System.Drawing.Point(221, 295);
+            this.buttonCrear.Name = "buttonCrear";
+            this.buttonCrear.Size = new System.Drawing.Size(149, 34);
+            this.buttonCrear.TabIndex = 9;
+            this.buttonCrear.Text = "Crear Registro";
+            this.buttonCrear.UseVisualStyleBackColor = false;
+            this.buttonCrear.Click += new System.EventHandler(this.buttonCrear_Click_1);
+            // 
+            // buttonSaludar
+            // 
+            this.buttonSaludar.BackColor = System.Drawing.SystemColors.Info;
+            this.buttonSaludar.Font = new System.Drawing.Font("Yu Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaludar.Location = new System.Drawing.Point(657, 372);
+            this.buttonSaludar.Name = "buttonSaludar";
+            this.buttonSaludar.Size = new System.Drawing.Size(103, 36);
+            this.buttonSaludar.TabIndex = 10;
+            this.buttonSaludar.Text = "Saludar";
+            this.buttonSaludar.UseVisualStyleBackColor = false;
+            this.buttonSaludar.Click += new System.EventHandler(this.buttonSaludar_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.label5.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(53, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(59, 22);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Notas";
+            // 
+            // textBoxNotas
+            // 
+            this.textBoxNotas.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxNotas.Location = new System.Drawing.Point(127, 234);
+            this.textBoxNotas.Name = "textBoxNotas";
+            this.textBoxNotas.Size = new System.Drawing.Size(356, 27);
+            this.textBoxNotas.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxNotas);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.buttonSaludar);
+            this.Controls.Add(this.buttonCrear);
+            this.Controls.Add(this.buttonBuscar);
+            this.Controls.Add(this.textBoxEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxSeccion);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxEstudiante);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxCarnet);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxCarnet;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxEstudiante;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxSeccion;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxEmail;
+        private System.Windows.Forms.Button buttonBuscar;
+        private System.Windows.Forms.Button buttonCrear;
+        private System.Windows.Forms.Button buttonSaludar;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxNotas;
+    }
+}
+
---
+++ miFormularioTarea/Form1.cs
@@ -0,0 +1,49 @@
+﻿using miFormularioTarea.Clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace miFormularioTarea
+{
+    public partial class Form1 : Form
+    {
+        Crud miCrud = new Crud();
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonBuscar_Click_1(object sender, EventArgs e)
+        {
+            textBoxEstudiante.Text = miCrud.MostrarInformacion(textBoxCarnet.Text);
+            textBoxNotas.Text = miCrud.MostrarTareas(textBoxCarnet.Text);
+            textBoxSeccion.Text = miCrud.MostrarInformacionSeccion(textBoxCarnet.Text);
+            textBoxEmail.Text = miCrud.MostrarInformacionCorreo(textBoxCarnet.Text);
+        }
+
+        private void buttonCrear_Click_1(object sender, EventArgs e)
+        {
+            string nombre = textBoxEstudiante.Text;
+            string carnet = textBoxCarnet.Text;
+            string email = textBoxEmail.Text;
+            string seccion = textBoxSeccion.Text;
+            string respuesta = miCrud.AgregarAlumno(carnet, nombre, email, seccion);
+            MessageBox.Show(respuesta);
+        }
+
+        private void buttonSaludar_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola te saludo desde el formulario🦁🦁🦁🦁");
+        }
+
+
+    }
+}
